b76e357ca6f637d0230f6d7f0191f803825c4f39
lenz <lensoff@gmail.com>
2020-03-16 11:26:13 +0300

    add tmux sh

---
+++ tmux.sh
@@ -0,0 +1,19 @@
+#tmux
+
+ctrl+b c - new window
+ctrl+b , - rename window
+ctrl+b p - previous window
+ctrl+b n - next window
+ctrl+b w - list windows
+ctrl+b % - split windows vertically
+ctrl+b " - split windows horizontally"
+
+tmux new -s <session name>
+
+ctrl+b d - detach from a session
+
+tmax list-sessions (or ls)
+tmax attach -t <session name>
+
+
+
